435be740c800632f464d0321628ffdc1cd466a8e
Faiq <faiq.mustaqeem@yahoo.com>
2019-03-07 15:37:05 +0500

    home added

--- src/main/java/com/edgeon/crud/SprinDemoCrud.java
+++ src/main/java/com/edgeon/crud/SprinDemoCrud.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
@@ -14,14 +16,22 @@ public class SprinDemoCrud {
 
     public static void main(String[] args) {
 
+
+
         SpringApplication.run(SprinDemoCrud.class, args);
 
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
     }
 
     @Bean
     CommandLineRunner init(StorageService storageService) {
         return (args) -> {
-//            storageService.deleteAll();
+            storageService.deleteAll();
             storageService.init();
         };
     }
